b8a228510788145f6c8b270034cb634c2bf71616
facundograffigna466 <facundograffigna466@outlook.com>
2025-11-08 21:39:19 -0300

    intenti pipeline -19

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
     supportFile: false,
     video: false,
     defaultCommandTimeout: 8000,
+    env: {
+      API_URL: process.env.API_URL || 'http://localhost:3001', // URL de backend local o de Render
+    },
   },
 })
-
